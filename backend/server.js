c3114e6fec916907188beac237a9aeed54fd280c
Jake Mullins <108378484+jake-mullins@users.noreply.github.com>
2024-10-13 11:51:50 -0600

    API auth boilerplate

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,34 @@
-const express       = require("express");
+const express   = require("express");
+const session   = require("express-session")
 const bodyParser    = require("body-parser");
+
 require("dotenv").config();
 require("./auth.js");
 
-// Authentication
-const passport  = require('passport');
-const GoogleStrategy  = require('passport-google-oauth20');
-
-
-// Routes
+// Routes import
 const apiRoutes  = require('./routes/api.js');
+const authRoutes = require('./routes/auth.js')
 
 const app   = express();
 
-app.use(bodyParser.json())
+// Session cookie
+const sess = {
+    secret: process.env.SECRET_KEY,
+    cookie: {}
+}
 
+if (process.env.SECRET_KEY === 'prod') {
+    app.set('trust proxy', 1)
+    sess.cookie.secure  = treu
+}
+
+// Middleware
+app.use(bodyParser.json())
+app.use(session(sess))
+
+// Routes
 app.use('/api', apiRoutes);
+app.use('/auth', authRoutes)
 
 const PORT  = process.env.PORT;
 
@@ -29,3 +42,5 @@ app.use(bodyParser.json())
 app.listen(PORT, "0.0.0.0", () => {
     console.log(`App is running on ${PORT}`)
 })
+
+module.exports.app  = app

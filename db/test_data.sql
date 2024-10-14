ff382138b30110b06bb38466f2efcfcadd2f5c3c
Jake Mullins <108378484+jake-mullins@users.noreply.github.com>
2024-10-13 18:01:58 -0600

    Modify User to use google auths

--- db/test_data.sql
+++ db/test_data.sql
@@ -1,14 +1,14 @@
-INSERT INTO "User" (username)
+INSERT INTO "User" (google_id, display_name, email)
 VALUES 
-('test1'),
-('test2'),
-('test3');
+('105737699977316017807', 'Jake Mullins', 'jakemullins0@outlook.com');
 
-INSERT INTO "IncidentLocal" (name, description)
+INSERT INTO "IncidentLocal" (owner_id, name, description)
 VALUES 
-('Moscow Shooting', 'April 22nd 2024, a shooter opened fire on a crowded theater in Moscow'),
-('Hawaii fires', 'October 26th 2023, wildfires raged in Lahaina, Hawaii destroying an entire neighborhood');
+(1, 'Moscow Shooting', 'April 22nd 2024, a shooter opened fire on a crowded theater in Moscow'),
+(1, 'Hawaii fires', 'October 26th 2023, wildfires raged in Lahaina, Hawaii destroying an entire neighborhood');
 
-INSERT INTO "Signal" (signalurl, metadata, latlon, notes, idincidentlocal) VALUES
-('https://x.com/p/0123456789abcdef', '', '-49.30003,50.33334', '@ananya_kumar filmed shooter from row 26b facing southeast. Footage is blurry, but gunshots can be heard. Footage taken on iPhone 12', 1),
-('https://facebook.com/p/123412341234', '', '-49.30023,50.32212', '@katerina filmed shooter from behind stage. Footage is able to identify shooter as a known Chechyan separatist', 1);
+INSERT INTO "Signal" (signal_url, metadata, lat_lon, notes, id_incident_local, owner_id)
+VALUES
+('https://x.com/p/0123456789abcdef', '', '-49.30003,50.33334', '@ananya_kumar filmed shooter from row 26b facing southeast. Footage is blurry, but gunshots can be heard. Footage taken on iPhone 12', 1, 1),
+('https://facebook.com/p/123412341234', '', '-49.30023,50.32212', '@katerina filmed shooter from behind stage. Footage is able to identify shooter as a known Chechyan separatist', 1, 1),
+('https://x.com/p/0123456789abcdef', 'Does not look super reliable', '-30.222,11.22323', '@jakemullins0_t Filmed multiple houses erupting into flames', 2, 1);
